83b03a7537bb3cfae65d06e8ea655b5a0a17f029
mireiaa-16 <mireiarino@gmail.com>
2026-03-27 10:39:58 +0100

    Admin updates

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import (Plant, User, Avatar, Inventory, Garden, Pot, PlantInGarden,
+                     FriendRequest, AlbumEntry, Image, Mission, UserMission, Station,
+                     WeatherReading, Shop)
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Plant)
+admin.site.register(Garden)
+admin.site.register(PlantInGarden)
+admin.site.register(Inventory)

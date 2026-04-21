24cf6e0c027fe5d423b796c5b9db85c0c93a9560
laiagesse <laia.gesse@estudiantat.upc.edu>
2026-04-21 15:31:51 +0200

    feat: inici_pocions

--- api/views/views_image.py
+++ api/views/views_image.py
@@ -3,7 +3,6 @@ import urllib
 
 import requests
 from django.core.files.base import ContentFile
-from rembg import remove
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -23,6 +22,7 @@ STATE_DESCRIPTIONS = {
 
 
 def createPlantImages(scientificName):
+    from rembg import remove
 
     safe_name = scientificName.replace(" ", "_").lower()
     plant = Plant.objects.get(scientificName=scientificName)

5ddb309c83995bbc5b47125fc166f17ce8b6f582
elena <elena.cai@estudiantat.upc.edu>
2026-05-17 20:00:32 +0200

    feat: adjuntar base de dades i funcions del servei amb el projecte principal i especificació openapi

--- third_party_service/models.py
+++ third_party_service/models.py
@@ -3,7 +3,6 @@ import re
 import secrets
 
 from django.core.exceptions import ValidationError
-from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -88,35 +87,3 @@ class ApiKey(models.Model):
                     )
                 }
             )
-
-
-class Station(models.Model):
-    stationCode = models.CharField(max_length=4, primary_key=True)  # RT.1
-    station = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    solarIrradiance = models.PositiveBigIntegerField()
-    temperature = models.FloatField()
-    precipitation = models.FloatField(validators=[MinValueValidator(0.0)])
-    updateDate = models.DateTimeField(auto_now=True)
-
-
-class WeatherReading(models.Model):
-    station = models.ForeignKey(
-        Station,
-        on_delete=models.CASCADE,
-        related_name="weather_readings",
-    )
-    timestamp = models.DateTimeField()
-    temperature = models.FloatField(null=True, blank=True)  # ºC       (var 32)
-    precipitation = models.FloatField(null=True, blank=True)  # mm       (var 35)
-    solarIrradiance = models.FloatField(null=True, blank=True)  # W/m²     (var 36)
-
-    class Meta:
-        unique_together = ("station", "timestamp")
-        indexes = [
-            models.Index(fields=["station", "timestamp"]),
-        ]
-        ordering = ["timestamp"]
-
-    def __str__(self):
-        return f"{self.station.stationCode} @ {self.timestamp}"

6fe1b78537bba1f7fa6d5aee9fbb00029226ba90
laiagesse <laia.gesse@estudiantat.upc.edu>
2026-05-17 17:52:53 +0200

    feat: descripcions al serializer

--- api/serializer.py
+++ api/serializer.py
@@ -91,18 +91,26 @@ class InventorySeedSerializer(serializers.Serializer):
     scientificName = serializers.CharField()
     amount = serializers.IntegerField()
     image_url = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
-    def get_image_url(self, obj):
+    def get_plant(self, obj):
         scientific_name = obj.get("scientificName")
 
         try:
-            plant = Plant.objects.get(scientificName=scientific_name)
+            return Plant.objects.get(scientificName=scientific_name)
         except Plant.DoesNotExist:
             return None
 
+    def get_image_url(self, obj):
+        plant = self.get_plant(obj)
+        if not plant:
+            return None
         image = Image.objects.filter(plant=plant, growthPhase="mature").first()
-
         return image.url.url if image and image.url else None
+
+    def get_description(self, obj):
+        plant = self.get_plant(obj)
+        return plant.description if plant else None
 
 
 class ShopSeedSerializer(serializers.Serializer):
@@ -127,14 +135,23 @@ class InventoryProductSerializer(serializers.Serializer):
     productName = serializers.CharField()
     amount = serializers.IntegerField()
     image_url = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
 
-    def get_image_url(self, obj):
+    def get_product(self, obj):
         product_name = obj.get("productName")
+
         try:
-            product = Product.objects.get(name=product_name)
+            return Product.objects.get(name=product_name)
         except Product.DoesNotExist:
             return None
-        return product.image_url.url if product.image_url else None
+
+    def get_image_url(self, obj):
+        product = self.get_product(obj)
+        return product.image_url.url if product and product.image_url else None
+
+    def get_description(self, obj):
+        product = self.get_product(obj)
+        return product.description if product else None
 
 
 class EventsCategorySerializer(serializers.ModelSerializer):

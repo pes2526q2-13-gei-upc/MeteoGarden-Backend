90d1901070eba64e3087cd6c78b2fd6a54bb430d
alexgomezz22 <alex.gomez.rodriguez@estudiantat.upc.edu>
2026-05-18 19:01:50 +0200

    feat: friends tests

--- api/tests/test_friend_request.py
+++ api/tests/test_friend_request.py
@@ -140,7 +140,6 @@ class FriendRequestTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # CAMBIO: Especificar accepted=True en la búsqueda para evitar ambigüedad
         req = FriendRequest.objects.get(
             requester=self.user2, requested=self.user1, accepted=True
         )
@@ -161,7 +160,6 @@ class FriendRequestTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        # CAMBIO: Especificar accepted=False en la búsqueda para evitar ambigüedad
         req = FriendRequest.objects.get(
             requester=self.user2, requested=self.user1, accepted=False
         )
@@ -223,41 +221,3 @@ class FriendRequestTests(APITestCase):
                 requester=self.user1, requested=self.user2
             ).exists()
         )
-
-    ############################
-    # GET REQUESTS
-    ############################
-
-    def test_get_sent_requests(self):
-        FriendRequest.objects.create(
-            requester=self.user1, requested=self.user2, accepted=None
-        )
-
-        response = self.client.generic(
-            "GET",
-            reverse("getRequests"),
-            data='{"action":"sent"}',
-            content_type="application/json",
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_received_requests(self):
-        FriendRequest.objects.create(
-            requester=self.user2, requested=self.user1, accepted=None
-        )
-
-        response = self.client.generic(
-            "GET",
-            reverse("getRequests"),
-            data='{"action":"received"}',
-            content_type="application/json",
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_get_invalid_action(self):
-
-        response = self.client.get(reverse("getRequests"), {"action": "cosa_rara"})
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)

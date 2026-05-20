75192a9f0866f565e3da179ed44f0576146927ef
elena <elena.cai@estudiantat.upc.edu>
2026-05-20 22:36:53 +0200

    feat: intent d'arreglar sonar

--- third_party_service/utils.py
+++ third_party_service/utils.py
@@ -1,6 +1,5 @@
 from third_party_service.models import ApiKey
 
-
 def generate_api_key(user, name=""):
-    api_key_obj, raw_token = ApiKey.issue_token(name=name, created_by=user)
+    api_key_obj, _ = ApiKey.issue_token(name=name, created_by=user)
     return api_key_obj
